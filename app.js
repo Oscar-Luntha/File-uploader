04ebd497a5d232a0aa7a365bc20ea2a018736076
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-25 22:44:29 +0200

    fix: correct import statement for authRouter and adjust middleware usage in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { fileURLToPath } from "node:url";
-
+import authRouter from "./routes/authRouter.js";
 import indexRouter from "./routes/indexRouter.js";
 
 const viewsPath = fileURLToPath(new URL("./views", import.meta.url));
@@ -8,11 +8,14 @@ const publicPath = fileURLToPath(new URL("./public", import.meta.url));
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", viewsPath);
 
 app.use(express.static(publicPath));
 app.use("/", indexRouter);
+app.use("/", authRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
