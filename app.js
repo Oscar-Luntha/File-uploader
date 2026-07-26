aa7eef7853db1f4409abde44e8c11818ed56ab67
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-26 22:29:26 +0200

    feat: add session management and authentication middleware

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import session from "express-session"
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
+
+import prisma from "./config/prisma.js";
+import passport from "./config/passport.js";
 import authRouter from "./routes/authRouter.js";
 import indexRouter from "./routes/indexRouter.js";
 
@@ -12,6 +18,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", viewsPath);
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    },
+    secret: process.env.SESSION_SECRET || "a_very_secret_key",
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // clean up expired sessions every 2 mins
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use(express.static(publicPath));
 app.use("/", indexRouter);
