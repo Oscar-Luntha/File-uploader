f493fbc4b015ccab6f93b7d0f60cd989b312cfbe
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-26 22:30:52 +0200

    add login routes to the authrouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getRegisterPage, postRegister } from "../controllers/authController.js";
+import { getRegisterPage, postRegister, getLoginPage, postLogin, logout,} from "../controllers/authController.js";
 
 const authRouter = Router();
-
+//register routes
 authRouter.get("/register", getRegisterPage);
 authRouter.post("/register", postRegister);
+//login routes
+authRouter.get("/login", getLoginPage);
+authRouter.post("/login", postLogin);
+//logout routes
+authRouter.get("/logout", logout);
 
 export default authRouter;
