af0e976a8987c9b055dd95506e8eb56ac2dbb316
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-27 14:33:47 +0200

    Disk storage configuration

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,3 +6,14 @@ const uploadDir = path.join(process.cwd(), "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+  },
+});
