1512368098c44022bba7828185a6d3878bde33ed
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-27 14:48:53 +0200

    download file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -43,3 +43,21 @@ export const uploadFile = async (req, res, next) => {
     next(error);
   }
 };
+export const downloadFile = async (req, res, next) => {
+  try {
+    const file = await prisma.file.findFirst({
+      where: {
+        id: req.params.id,
+        userId: req.user.id,
+      },
+    });
+
+    if (!file || !file.path) {
+      return res.status(404).send("File not found.");
+    }
+
+    res.download(file.path, file.name);
+  } catch (error) {
+    next(error);
+  }
+};
