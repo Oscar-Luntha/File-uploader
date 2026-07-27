866a8f63de456e8ab598c3d0f4e542d27ce62bba
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-27 14:12:56 +0200

    fix dashboard router

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -3,7 +3,7 @@ import { getDashboard } from "../controllers/dashboardController.js";
 import { isAuth } from "../middleware/auth.js";
 
 const dashboardRouter = Router();
-
+dashboardRouter.use(isAuth)
 dashboardRouter.get("/dashboard", isAuth, getDashboard);
 
 export default dashboardRouter;
